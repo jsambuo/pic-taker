eef77b0a6e78bb87af847c416483daaa3055e70e
jsambuo <jsambuo@gmail.com>
2013-11-09 18:21:46 -0500

    Adding code for CameraPreview, saving image, and fixing package typo

--- src/com/sambuo/pictaker/MainActivity.java
+++ src/com/sambuo/pictaker/MainActivity.java
@@ -1,12 +1,15 @@
-package com.sambuo.pictacker;
+package com.sambuo.pictaker;
 
 import com.kinvey.android.Client;
 import com.kinvey.android.callback.KinveyPingCallback;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -25,6 +28,14 @@ public class MainActivity extends Activity {
 		        Log.d("Test", "Kinvey Ping Success");
 		    }
 		});
+		
+		final Button button = (Button) findViewById(R.id.buttonTakePicture);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intentTakePicture = new Intent(v.getContext(), TakePictureActivity.class);
+                startActivity(intentTakePicture);
+            }
+        });
 	}
 
 	@Override
