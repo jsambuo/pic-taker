bd5afe216c891c574613c2eb3c31d1efb4b3fd03
jsambuo <jsambuo@gmail.com>
2013-11-09 13:17:20 -0500

    Adding Kinvey

--- src/com/sambuo/pictacker/MainActivity.java
+++ src/com/sambuo/pictacker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.sambuo.pictacker;
 
+import com.kinvey.android.Client;
+import com.kinvey.android.callback.KinveyPingCallback;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +14,17 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Client mKinveyClient = new Client.Builder("kid_PTxf3dWkUO", "c41dcceb19d24cf09f347c30397fea6d", this.getApplicationContext()).build();
+		
+		mKinveyClient.ping(new KinveyPingCallback() {
+		    public void onFailure(Throwable t) {
+		        Log.e("Test", "Kinvey Ping Failed", t);
+		    }
+		    public void onSuccess(Boolean b) {
+		        Log.d("Test", "Kinvey Ping Success");
+		    }
+		});
 	}
 
 	@Override
